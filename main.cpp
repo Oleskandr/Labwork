0e4b291fe778e0555720a8b93a7a420d036f912a
Oleskandr <141867436+Oleskandr@users.noreply.github.com>
2023-10-05 20:13:49 +0000

    First_error

--- main.cpp
+++ main.cpp
@@ -2,22 +2,34 @@
 using namespace std;
 
 int main() {
-
+    
+    bool key = false;
+    
+    first_error: 
+    if(key == true)
+    {
+        cout << "Error, it must be a natural number\n";
+        key = false;
+    }
+    
     int n, m, sum = 0;
 
     cout << "Enter number n: ";
     cin >> n;
     cout << "Enter number m: ";
     cin >> m;
-
-    cout << "Numbers that are multiples 4:\t";
     
+    if (n <= 0 || m <= 0)
+    {
+        key = true;
+        goto first_error;
+    }
+
     for(int i = m; i < n; i++)
     {
         if(i % 4 == 0)
         {
             sum += i;
-            printf("%d,\t",i);
         }
     }
 
